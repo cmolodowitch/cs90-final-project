f67354710ee11409e9fe7d56fc6b9292e246520f
Christina Molodowitch <cmolodowitch@bwh.harvard.edu>
2023-12-13 23:18:16 -0500

    Refactor for BaseEntity move, add logging
    Refactored TodoServiceImpl for BaseEntity package change.  Added logging to service methods.

--- src/main/java/org/molodowitch/cs90/todos/services/TodoServiceImpl.java
+++ src/main/java/org/molodowitch/cs90/todos/services/TodoServiceImpl.java
@@ -1,10 +1,12 @@
 package org.molodowitch.cs90.todos.services;
 
-import org.molodowitch.cs90.todos.BaseEntity;
+import org.molodowitch.cs90.todos.model.BaseEntity;
 import org.molodowitch.cs90.todos.model.TodoItem;
 import org.molodowitch.cs90.todos.model.TodoList;
 import org.molodowitch.cs90.todos.repositories.TodoItemRepository;
 import org.molodowitch.cs90.todos.repositories.TodoListRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @Service
 public class TodoServiceImpl implements TodoService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TodoServiceImpl.class);
 
     private final TodoListRepository listRepository;
     private final TodoItemRepository itemRepository;
@@ -25,31 +29,38 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public List<TodoList> getAllLists() {
+        LOG.debug("Retrieving all to-do lists");
         return BaseEntity.convertIterableToList(listRepository.findAll());
     }
 
     @Override
     public Optional<TodoList> getList(Long listId) {
+        LOG.debug("Retrieving to-do list {}", listId);
         return listRepository.findById(listId);
     }
 
     @Override
     public TodoList saveList(TodoList list) {
+        LOG.debug("Saving to-do list {} with name {}", list.getId(), list.getName());
         return listRepository.save(list);
     }
 
     @Override
     public TodoItem editItem(TodoItem item) {
+        LOG.debug("Updating to-do item {}", item.getId());
         return itemRepository.save(item);
     }
 
     @Override
     public boolean deleteItem(Long itemId) {
+        LOG.info("Deleting to-do item {}", itemId);
         if (itemRepository.existsById(itemId)) {
+            LOG.debug("To-do item {} exists", itemId);
             itemRepository.deleteById(itemId);
             return true;
         }
         else {
+            LOG.warn("Attempted to delete non-existent to-do item {}", itemId);
             return false;
         }
     }
