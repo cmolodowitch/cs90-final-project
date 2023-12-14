60f8220ac8b665fce89ac0633884241f1cd21ecc
Christina Molodowitch <cmolodowitch@bwh.harvard.edu>
2023-12-13 23:31:28 -0500

    Add javadoc

--- src/main/java/org/molodowitch/cs90/todos/services/TodoService.java
+++ src/main/java/org/molodowitch/cs90/todos/services/TodoService.java
@@ -8,13 +8,46 @@ import java.util.Optional;
 
 public interface TodoService {
 
+    /**
+     * Retrieves all saved to-do lists with their items.
+     *
+     * @return List containing all saved to-do lists with their items,
+     *         or an empty List if there are no saved to-do lists
+     */
     List<TodoList> getAllLists();
 
+    /**
+     * Retrieves the to-do list with the specified listId persistence index, if any.
+     *
+     * @param listId persistence index of the to-do list to retrieve
+     * @return Optional containing the to-do list with the specified persistence index,
+     *         or an empty Optional if there is no such entry
+     */
     Optional<TodoList> getList(Long listId);
 
+    /**
+     * Saves the specified to-do list, returning the saved entity.
+     *
+     * @param list TodoList containing data to be saved.
+     * @return saved TodoList entity
+     */
     TodoList saveList(TodoList list);
 
+    /**
+     * Saves the changes to the specified to-do item, returning the saved entity.
+     *
+     * @param item TodoItem containing changes to be made.
+     * @return saved TodoItem entity
+     */
     TodoItem editItem(TodoItem item);
 
+    /**
+     * Deletes the to-do item with the specified persistence index.
+     * Does nothing if there's no such entry.
+     *
+     * @param itemId persistence index of the to-do item to delete
+     * @return true if a to-do item was deleted, false if there's no item with the
+     *         specified persistence index
+     */
     boolean deleteItem(Long itemId);
 }
